ff236d42e059a6bdc3848c88b79b921d4e43815f
Märt Põder <boamaod@gmail.com>
2013-06-26 23:56:15 +0300

    Formatting fixes to et.php

--- et.php
+++ et.php
@@ -1,30 +1,30 @@
 <?php
 
 $menu = <<<EOD
-  <li><a href="#">Add your signature</a></li>
-  <li><a href="#">About the Pirate movement</a></li>
+  <li><a href="#">Lisa oma allkiri</a></li>
+  <li><a href="#">Piraadiliikumisest</a></li>
 EOD;
 
 $manifesto = <<<EOD
   <div class="first box">
-    <p><span class="intro">Oleme vapustatud</span>, et Interneti kasutajate ülemaailmne jälgimine PRISMi jt inimajaloos pretsedentitute programmide kaudu on osutunud tõeks. Sellise lausjälgimisvõimekuse rakendamine — eriti kui seda tehakse kodanikkonna kriitilise järelvalveta — kujutab endast tõsist ohtu inimõigustele nagu sõnavabadus ja privaatsus, ning seeläbi demokraatia alustaladele üldse.<br /><br /></p>
-    <p><span class="intro">Meie poolt suur aplaus vilepuhuja</span>, Edward Snowdenile, kes need andmed avalikustas. Kui valitsus on tõepoolest ellu kutsutud kodanike poolt ja kodanike jaoks, siis ei saa olla kuritegu lekitada andmeid valitsuse tegutsemisviiside ega nende ulatuse kohta, iseäranis kui valitsus väidab end tegevat seda kodanike nimel ja nende kaitseks. Esindusdemokraatia nurgakiviks on rahva enamuse heakskiit valitud esindajate tegevusele. Ent selline heakskiit pole võimalik, kui info oma tegevuste kohta hoitakse salajas.</p>
+    <p><span class="intro">Oleme vapustatud</span>, et <strong>netikasutajate ülemaailmne jälgimine</strong> PRISMi ja teiste inimajaloos pretsedentitute programmide kaudu on osutunud tõeks. Sellise lausjälgimisvõimekuse rakendamine — eriti kuna seda tehakse kodanikkonna kriitilise järelvalveta — <strong>kujutab endast tõsist ohtu inimõigustele nagu sõnavabadus ja privaatsus, ning seeläbi demokraatia alustaladele üldse</strong>.<br /><br /></p>
+    <p><span class="intro">Meie poolt suur aplaus vilepuhuja Edward Snowdenile</span>, kes need andmed avalikustas. Kui valitsus on tõepoolest ellu kutsutud kodanike poolt ja kodanike jaoks, siis ei saa olla kuritegu lekitada andmeid valitsuse tegutsemisviiside ega nende ulatuse kohta, iseäranis kui valitsus väidab end tegevat seda kodanike nimel ja nende kaitseks. Esindusdemokraatia nurgakiviks on rahva enamuse heakskiit valitud esindajate tegevusele. Ent selline heakskiit pole võimalik, kui info oma tegevuste kohta hoitakse salajas.</p>
     <p>
 Täheldame murega täielikku hoolimatust, mida USA valitsus näitab üles Euroopa kui ka muude maade kodanike suhtes, kes kasutavad ühendriikides baseeruvaid sideteenuseid ja -taristut. Ühtlasi rõhutame selle negatiivset mõju ühendriikide liitlastele, asjassepuutuvate riikide suveräänsusele ja nende majanduste konkurentsivõimele.
 </p>
     <p><span class="intro">
-Euroopa peab ilmnenud asjaoludele reageerima</span> piisava otsustavusega. On tähtis, et Euroopa Liit mitte ei jääks ses olukorras kaassüüdlaseks võimu kuritarvitamises — see oleks kahju, millega kodanikuühiskond poleks valmis leppima —, vaid võtaks endale teenäitaja rolli digikeskkonna õiguste ja privaatsuse, valitsemise läbipaistvuse ja vilepuhujatest avalikustajate kaitsel.<br /><br /></p>
+Euroopa peab reageerima</span> ilmnenud asjaoludele piisava otsustavusega. On tähtis, et Euroopa Liit mitte ei jääks ses olukorras kaassüüdlaseks võimu kuritarvitamises — see oleks kahju, millega kodanikuühiskond poleks valmis leppima —, vaid võtaks endale <strong>teenäitaja rolli digikeskkonna õiguste ja privaatsuse, valitsemise läbipaistvuse ja vilepuhujatest avalikustajate kaitsel</strong>.<br /><br /></p>
     <p><span class="intro">Allakirjutanute nõudmised on:</span></p>
   </div>            
   <div class="box">
-    <h3><a href="#" onclick="return toggle(this)">1. Varjupaik ja kaitse avalikustajatele
+    <h3><a href="#" onclick="return toggle(this)">1. Kaitse avalikustajatele
 </a></h3>
     <div class="expanded">
       <p>
-Ühendriikide valitsuse käitumine Bradley Manningu ja teiste avalikkuses häirekella löönud lekitajatega on tõsiselt murettekitav. Edward Snowdeni juba ette “reeturiks” sildistamine eri USA ametiisikute ja meediaväljaannete poolt on loonud olukorra, kus ta ei saa loota erapooletule kohtumõistmisele. Tema poliitiline veendumus valitsemise läbipaistvusest on osutunud hukkamõistu ajendiks ja valitseb selge oht, et ta langeb ebainimliku või alandava kohtlemise või karistuse ohvriks, kuni surmanuhtluseni välja.
+Ühendriikide valitsuse käitumine Bradley Manningu ja teiste avalikkuses häirekella löönud lekitajatega on tõsiselt murettekitav. Edward Snowdeni juba ette “reeturiks” sildistamine eri USA ametiisikute ja meediaväljaannete poolt on loonud olukorra, kus ta ei saa loota erapooletule kohtumõistmisele. Tema poliitiline veendumus valitsemise läbipaistvusest on osutunud hukkamõistu ajendiks ning valitseb selge oht, et ta langeb ebainimliku või alandava kohtlemise või karistuse ohvriks — kuni surmanuhtluseni välja.
 </p>
       <p>
-Kutsume kõiki Euroopa valitsusi üles käsitlema heast tahtest lähtuvalt Edward Snowdeni ning teiste vilepuhujatest avalikustajate poliitilise varjupaiga ja täiendava kaitse seisundi taotlusi ning vaatama sellised taotlused läbi kiirendatud korras.
+Kutsume kõiki Euroopa valitsusi üles <strong>käsitlema heast tahtest lähtuvalt Edward Snowdeni ning teiste vilepuhujatest avalikustajate poliitilise varjupaiga ja täiendava kaitse seisundi taotlusi</strong> ning vaatama sellised taotlused läbi kiirendatud korras!
 </p>
     </div>
   </div>
@@ -32,7 +32,7 @@ Kutsume kõiki Euroopa valitsusi üles käsitlema heast tahtest lähtuvalt Edwar
     <h3><a href="#" onclick="return toggle(this)">2. Faktide väljaselgitamine</a></h3>
     <div class="expanded">
       <p>
-On vastuvõetamatu, et salajane jälitusvõimekus ja sellele vastav praktika lähevad mööda demokraatliku asjaajamise nõuetest ning seavad takistusi ühiskonna ratsionaalsele ja kriitilisele panusele, mis on vajalik, et üksteisest eristada demokraatia raames kohased ja kohatud teguviisid.
+On vastuvõetamatu, et salajane jälitusvõimekus ja sellele vastav praktika lähevad mööda <strong>demokraatliku asjaajamise</strong> nõuetest ning seavad takistusi ühiskonna ratsionaalsele ja kriitilisele panusele, mis on vajalik, et teineteisest eristada demokraatia raames kohased ja kohatud teguviisid.
 </p>
       <p>
 Kutsume Euroopa Parlamenti vastavalt kodukorra artiklile 185 üles looma <strong>uurimiskomisjoni</strong>. Oluline on teha kindlaks ja avalikustada järgnevad faktid:
@@ -44,7 +44,7 @@ Kutsume Euroopa Parlamenti vastavalt kodukorra artiklile 185 üles looma <strong
         <li>Missugusel määral on rikutud põhiõiguste harta, andmekaitse direktiivi, eraelu puutumatust ja elektroonilist sidet käsitleva direktiivi või teiste EL seaduste sätteid?</li>
       </ul>
       <p>
-Laiendame seda üleskutset kõigi riikide parlamentidele — kutsume teid üles kindlaks tegema, kas ja mil määral on rikutud riikide põhiseadusi või andmekaitset ning jälitustegevust puudutavaid seadusi.
+Laiendame seda üleskutset kõigi riikide parlamentidele — kutsume teid üles kindlaks tegema, kas ja mil määral on rikutud riikide põhiseadusi või andmekaitset ning jälitustegevust puudutavaid seadusi!
 </p>
     </div>
   </div>
@@ -55,19 +55,19 @@ Laiendame seda üleskutset kõigi riikide parlamentidele — kutsume teid üles 
 Kujundamisel olevat terviklikku isikuandmete kaitse regulatsiooni tuleb tugevdada, et see tagaks laiapõhjalise ja ulatusliku kaitse isiklikele ning ärilistele andmetele. Vastupidist taotlevatele kuluaaripoliitika pingutustele tuleb otsustavalt vastu seista.
 </p>
       <p>
-Nõuame, et <strong>Euroopa riikide kodanike andmeid ei edastataks teadlikult USA luureorganisatsioonidele</strong>. On vaja ennistada isikuandmete kaitse regulatsiooni esimese lekkinud kavandi artikkel 42, mis puudutab kolmandate riikide territooriumivälist tegutsemist viisil, nagu seda teevad ühendriikide Patriot Act ja Foreign Intelligence Surveillance Act, ja piirab välisriikide õigusorganite ligipääsu Euroopa andmetele. Kaitstud peavad olema ka metaandmed ning pseudonüümselt esitatud andmed.
+Nõuame, et <strong>Euroopa riikide kodanike andmeid ei edastataks teadlikult USA luureorganisatsioonidele</strong>. On vaja ennistada isikuandmete kaitse regulatsiooni esimese lekkinud kavandi artikkel 42, mis puudutab kolmandate riikide territooriumivälist tegutsemist viisil, nagu seda teevad ühendriikide <i>Patriot Act</i> ja <i>Foreign Intelligence Surveillance Act</i>, ja piirab välisriikide õigusorganite ligipääsu Euroopa andmetele. Kaitstud peavad olema ka metaandmed ning pseudonüümselt esitatud andmed.
 </p>
       <p>
-Vastavalt rahvusvahelistele Safe Harbor privaatsuse tagamise põhimõtetele on ühendriikide äriettevõtted kohustatud kasutajat informeerima, kui tema andmeid edastatakse kolmandatele osapooltele. On ilmne, et PRISM-programmis osalevad ettevõtted on neid kohustusi rikkunud. Vastusena peab Euroopa Liit taandama <strong>end neist kokkulepetest</strong> (komisjoni otsus 2000/520/EC), et ettevõtteid, mis säärast praktikat koheselt ei lõpeta, oleks võimalik Euroopa kohtutes vastutusele võtta. Safe Harbor tuleb saata uuele läbirääkimiste ringile, et luua senisest tõhusamad kaitse- ja tugimehanismid või asendada need uute rahvusvaheliste andmekaitse lepetega, mis põhinevad plaanitaval terviklikul isikuandmete kaitse regulatsioonil.
+Vastavalt rahvusvahelistele <i>Safe Harbor</i> privaatsuse tagamise põhimõtetele on ühendriikide äriettevõtted kohustatud kasutajat informeerima, kui tema andmeid edastatakse kolmandatele osapooltele. On ilmne, et PRISM-programmis osalevad ettevõtted on neid kohustusi rikkunud. Vastusena peab Euroopa Liit <strong>taandama end neist kokkulepetest</strong> (komisjoni otsus 2000/520/EC), et ettevõtteid, mis säärast praktikat koheselt ei lõpeta, oleks võimalik Euroopa kohtutes vastutusele võtta. <i>Safe Harbor</i> tuleb saata uuele läbirääkimiste ringile, et luua senisest tõhusamad kaitse- ja tugimehanismid või asendada need uute rahvusvaheliste andmekaitse lepetega, mis põhinevad terviklikul isikuandmete kaitse regulatsioonil.
 </p>
     </div>
   </div>
   <div class="box">
-    <h3><a href="#" onclick="return toggle(this)">4. Rahvusvahelise Interneti-vabaduste leppe saavutamine
+    <h3><a href="#" onclick="return toggle(this)">4. Rahvusvaheline Interneti-vabaduste lepe
 </a></h3>
     <div class="expanded">
       <p>
-Tagamaks, et Internet oleks jätkuvalt võrdsustav ja demokratiseeriv jõud ega kujuneks edaspidi vahendiks demokraatia ja isikuvabaduste piiramisel või kärpimisel, selleks peab Euroopa Liit suunama oma pingutused rahvusvahelise <strong>Interneti-vabaduste leppe saavuta</strong> amisele. Selline lepe peab tõhusalt tagama nii sõnumisaladuse, sõnavabaduse ja vaba ligipääsu informatsioonile (sh Internetile spetsiifilisel viisil) kui ka võrguneutraalsuse printsiibi järgimise.
+Tagamaks, et Internet oleks jätkuvalt võrdsustav ja demokratiseeriv jõud ega kujuneks edaspidi vahendiks demokraatia ja isikuvabaduste piiramisel või kärpimisel, selleks peab Euroopa Liit suunama oma pingutused <strong>rahvusvahelise Interneti-vabaduste leppe</strong> saavutamisele. Selline lepe peab tõhusalt tagama nii sõnumisaladuse, sõnavabaduse ja vaba ligipääsu informatsioonile (sh Internetile spetsiifilisel viisil) kui ka võrguneutraalsuse printsiibi järgimise.
 </p>
     </div>
   </div>
@@ -75,10 +75,10 @@ Tagamaks, et Internet oleks jätkuvalt võrdsustav ja demokratiseeriv jõud ega 
     <h3><a href="#" onclick="return toggle(this)">5. Privaatsusteadliku tarkvara rahastamine</a></h3>
     <div class="expanded">
       <p>
-Privaatsuse täiendava kaitseliinina peab kasutajatel olema võimalus kasutada <strong>tarkvara ja teenuseid, mis tagavad neile tugeva sõnumisaladuse ja privaatsuse kaitse</strong>. Selline tarkvara võib muuhulgas pakkuda anonüümsust, lõppkasutajate vahelist krüpteeringut, võrdõigusvõrgustiku arhitektuuri, võimalust hoiustada kasutaja andmeid ühiselt või teistest sõltumatult, auditeeritavat ja avatud lähtekoodi ning muid privaatsuskaitse võimalusi.
+Privaatsuse täiendava kaitseliinina peab kasutajatel olema võimalus <strong>kasutada tarkvara ja teenuseid, mis tagavad neile tugeva sõnumisaladuse ja privaatsuse kaitse</strong>. Selline tarkvara võib muuhulgas pakkuda anonüümsust, lõppkasutajate vahelist krüpteeringut, võrdõigusvõrgustiku arhitektuuri, võimalust hoiustada kasutaja andmeid ühiselt või teistest sõltumatult, auditeeritavat ja avatud lähtekoodi ning muid privaatsuskaitse võimalusi.
 </p>
       <p>
-Meid rõõmustab asjaolu, et praegune Horizon 2020 raamistiku programmikavand sisaldab eesmärgina “tagada privaatsus ja vabadus Internetis”. Kutsume Euroopa Liitu kindlustama, et seda eesmärki toetava tarkvara- ja teenuste mitmekesisuse tagamiseks eraldataks märgatavalt rohkem uurimistoetusi kui vastupidise eesmärgiga projektidele, so jälitus- ja andmekaeve-tehnoloogiate uurimisprojektidele. Täielikult tuleks loobuda selliste projektide rahastamisest, mille väljendatud eesmärk on eristusvõimetu ja konkreetse kahtlusaluseta jälgimis- ning jälitustegevus.
+Meid rõõmustab asjaolu, et praegune <i>Horizon 2020</i> raamistiku programmikavand sisaldab eesmärgina “tagada privaatsus ja vabadus Internetis”. Kutsume Euroopa Liitu kindlustama, et seda eesmärki toetava tarkvara- ja teenuste mitmekesisuse tagamiseks eraldataks <strong>märgatavalt rohkem uurimistoetusi</strong> kui vastupidise eesmärgiga projektidele, so jälitus- ja andmekaeve-tehnoloogiate uurimisprojektidele. Täielikult tuleks loobuda selliste projektide rahastamisest, mille väljendatud eesmärk on eristusvõimetu ja konkreetse kahtlusaluseta jälgimis- ning jälitustegevus.
 
 </p>
     </div>          
@@ -88,12 +88,12 @@ Meid rõõmustab asjaolu, et praegune Horizon 2020 raamistiku programmikavand si
 </a></h3>
     <div class="expanded">
       <p>
-Pakume välja seadusandlikud meetmed, et tugevdada kaitset taoliste tegevusvahendite võimuliialduste vastu Euroopas.</p>
+Pakume välja seadusandlikud meetmed, et <strong>tugevdada kaitset taoliste tegevusvahendite võimuliialduste vastu Euroopas</strong>.</p>
       <p>
-Riiklike agentuuride <strong>otseühendused</strong> Interneti-taristu magistraalvõrku — nagu neid paigaldas väidetavalt BLARNEY-programmi raames NSA — peavad olema seadusega ühemõtteliselt välistatud. Sellised otseühendused võimaldavad kogu Interneti-andmeside salvestamist ning selle hilisemat andmekaevet ja niimoodi minnakse mööda seadusega määratud piirangutest ning protseduuridest, rikkudes sõnumisaladust kõigi edastavate andmete ja isikute puhul. Sellisel vastuvõetamatul moel võrgutaristu toimimisse sekkumine kahandab kogu Interneti usaldusväärsust ning seab ohtu kõik selle poolt võimaldatud hüved.
+Riiklike agentuuride <strong>otselülitused</strong> Interneti-taristu magistraalvõrku — nagu neid paigaldas väidetavalt BLARNEY-programmi raames NSA — peavad olema seadusega ühemõtteliselt välistatud. Sellised otselülitused võimaldavad kogu Interneti-andmeside salvestamist ning selle hilisemat andmekaevet ja niimoodi minnakse mööda seadusega määratud piirangutest ning protseduuridest, rikkudes sõnumisaladust kõigi edastavate andmete ja isikute puhul. Sellisel vastuvõetamatul moel võrgutaristu toimimisse sekkumine kahandab kogu Interneti usaldusväärsust ning seab ohtu kõik selle poolt võimaldatud hüved.
 </p>
       <p>
-Kooskõlas eelevaga kutsume üles tühistama ka <strong>andmesäilitusdirektiivi</strong>. Tšehhi ja Rumeenia põhiseaduskohtud on ühemõtteliselt otsustanud, et eristusvõimetu ja konkreetse kahtlusaluseta andmesäilitus läheb vastuollu põhiliste inimõigustega. Andmesäilitusprogrammid, mille raames kohtu loata koondatakse laiaulatuslikult ja valimatult andmebaasidesse sõnumisaladusega kaitstud andmeid, panevad aluse täitevvõimu sellisele võimuliialdustele, mis viib platvormide nagu PRISM loomiseni. Sellised arengud on otseseks ohuks seadusandliku võimu ja täidesaatava võimu lahususele, mis on meie demokraatliku riigikorralduse aluseks.
+Kooskõlas eelevaga kutsume ka üles <strong>tühistama kehtiva andmesäilitusdirektiivi</strong>. Tšehhi ja Rumeenia põhiseaduskohtud on ühemõtteliselt otsustanud, et eristusvõimetu ja konkreetse kahtlusaluseta andmesäilitus läheb vastuollu põhiliste inimõigustega. Andmesäilitusprogrammid, mille raames kohtu loata koondatakse laiaulatuslikult ja valimatult andmebaasidesse sõnumisaladusega kaitstud andmeid, panevad aluse täitevvõimu sellistele võimuliialdustele, mis viib platvormide nagu PRISM loomiseni. Sellised arengud on otseseks ohuks seadusandliku võimu ja täidesaatava võimu lahususele, mis on meie demokraatliku riigikorralduse aluseks.
 </p>
     </div>          
   </div>
